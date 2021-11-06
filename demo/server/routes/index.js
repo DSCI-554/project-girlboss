48e4b0f859473d24a3e2c4e3cac31aa2b1a66d0d
Erica Xia <ericaxia3@gmail.com>
2021-11-05 18:30:09 -0700

    create backend server

---
+++ demo/server/routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'DSCI 554 Group Project - Backend Server' });
+});
+
+module.exports = router;
