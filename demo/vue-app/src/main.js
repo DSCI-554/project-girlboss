fda947dbba74debdd405322559cd5744d6952c5b
Erin Szeto <erinszet@usc.edu>
2021-11-09 11:42:53 -0800

    import BootstrapVue in main.js

--- demo/vue-app/src/main.js
+++ demo/vue-app/src/main.js
@@ -2,6 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import '../node_modules/bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
 
