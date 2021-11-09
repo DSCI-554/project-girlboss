4b8c21f70ed1c91da450acb5cf6e1196a37f9e97
Erin Szeto <erinszet@usc.edu>
2021-11-09 11:16:40 -0800

    add dashboard vue page

---
+++ demo/vue-app/src/views/Dashboard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="dashboard">
+      Welcome to the Dashboard
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+// export default {
+//   name: 'Home',
+//   components: {
+//     HelloWorld
+//   }
+// }
+</script>
