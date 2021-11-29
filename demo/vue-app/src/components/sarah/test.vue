e3e44d18c90306521708431d37ae66f210100ca1
sarahpursley <spursley@usc.edu>
2021-11-28 21:41:39 -0800

    Create test.vue

---
+++ demo/vue-app/src/components/sarah/test.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="tsdiv">
+    <h1 align="left">Education And STEM</h1>
+    <b-row align-h="start">
+      <b-col align-self="start" cols="8">
+        <!-- <p align="left">
+          Gender equality is crucial to economic growth and sustainable
+          development, including equality in the workplace to work and provision
+          of fair pay. Women still face significant barriers to work and access
+          to decision-making roles and jobs all across the world.
+        </p> -->
+      </b-col>
+    </b-row>
+
+    <!-- <div ref="cpchart" align="center"></div> -->
+    <!-- <div id="svgcontainer"></div> -->
+    <svg width="932" height="932"></svg>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
