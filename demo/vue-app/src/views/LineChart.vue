37c9152dd4d6d26ecccceea113b8fd64c4baccce
Erin Szeto <erinszet@usc.edu>
2021-12-03 12:49:11 -0800

    change layout of page to match sidebar and correct redirects

--- demo/vue-app/src/views/LineChart.vue
+++ demo/vue-app/src/views/LineChart.vue
@@ -2,54 +2,31 @@
   <div class="dashboard">
     <div class="container-fluid">
       <div class="row">
-<nav
+        <nav
           id="sidebarMenu"
           class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse"
         >
           <div class="sidebar-sticky pt-3">
             <ul class="nav flex-column">
-              <b align="left">Employment</b>
-              <hr>
-              <li class="nav-item">
-                <a class="nav-link" align="left" href="/wagegap">
-                  Gender Wage Gap Around the World
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" align="left" href="/monthlywages">
-                  Monthly Wages by Gender Over Time
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" align="left" href="/employmentbysector">
-                  Employment by Sector
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" align="left" href="/mapboxmap">
-                  Median Annual Income of US Women
-                </a>
-              </li>
-              <br>
               <b align="left">Education</b>
-              <hr>
-                <li class="nav-item">
-                <a class="nav-link" align="left" href="/barchart">
+              <hr />
+              <li class="nav-item">
+                <a class="nav-link" align="left" href="/education">
                   Global Student Enrollment
                 </a>
               </li>
-            <li class="nav-item">
-                <a class="nav-link" align="left" href="/linechart">
+              <li class="nav-item">
+                <a class="nav-link active" align="left">
                   Gender Parity Indices in Tertiary Education
                 </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" align="left" href="/bubblechart">
-                STEM Graduates and Median Salaries
+                <a class="nav-link" align="left" href="/education-stem-salaries">
+                  STEM Graduates and Median Salaries
                 </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link active" align="left" href="/radialchart">
+                <a class="nav-link" align="left" href="/education-stem-graduates">
                   STEM Distribution of US Women
                 </a>
               </li>
@@ -57,38 +34,83 @@
           </div>
         </nav>
 
-        <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
-          <div
-            class="
-              d-flex
-              justify-content-between
-              flex-wrap flex-md-nowrap
-              align-items-center
-              pt-3
-              pb-2
-              mb-3
-              border-bottom
-            "
-          >
-
-            <h4>Visualizing the Gender Gap in Employment and Education</h4> 
-          </div>
-
-        <br>
-        <!-- Timeseries (Multiseries Line Chart) -->
-        <LineChart />
-        
-        </main>
+          <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
+            <div class="
+                  d-flex
+                  justify-content-between
+                  flex-wrap flex-md-nowrap
+                  align-items-center
+                  pt-3
+                  pb-2
+                  mb-3
+                  border-bottom
+                "
+                style="margin-left: 212px"
+            >
+                <h4>Visualizing the Gender Gap in Employment and Education</h4>
+            </div>
+            <div style="margin-left:30px">
+            <LineChart />
+            </div>
+            <!-- Insert components here -->
+          </main>
+        <!-- </div> -->
       </div>
     </div>
   </div>
 </template>
 
-<style scoped>
+<style>
 .nav-link.active,
 .nav-link:hover,
-.nav-link:focus{
-    color: #111;
+.nav-link:focus {
+  color: #111;
+}
+
+#line-button {
+  margin-bottom: 4%;
+  margin-right: 20%;
+}
+
+.sidebar {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 100; /* Behind the navbar */
+  padding: 48px 0 0; /* Height of navbar */
+  box-shadow: inset -1px 0 0 rgba(0, 0, 0, .1);
+}
+
+.sidebar-sticky {
+  position: relative;
+  top: 0;
+  height: calc(100vh - 48px);
+  padding-top: .5rem;
+  overflow-x: hidden;
+  overflow-y: auto; /* Scrollable contents if viewport is shorter than content. */
+}
+
+@supports ((position: -webkit-sticky) or (position: sticky)) {
+  .sidebar-sticky {
+    position: -webkit-sticky;
+    position: sticky;
+  }
+}
+
+/* #main_div {
+  margin-left: 175px;
+} */
+@media only screen and (min-width : 320px) {
+  #main_div {
+    margin-left: 225px;
+  }
+}
+
+@media only screen and (min-width : 1400px) {
+  #main_div {
+    margin-left: 300px;
+  }
 }
 </style>
 
